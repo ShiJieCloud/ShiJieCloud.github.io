081622b11d685d3b525f0bd1b881e43157e310ac
ShiJieCloud <53214666+ShiJieCloud@users.noreply.github.com>
2023-06-04 07:56:17 +0000

    add post nacos Updated By Github Actions

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,1 +1,1 @@
-var posts=["posts/29513.html","posts/35392.html","posts/55151.html","posts/52064.html","posts/35821.html","posts/35916.html","posts/11052.html","posts/35966.html"];function toRandomPost(){pjax.loadUrl("/"+posts[Math.floor(Math.random()*posts.length)])}
+var posts=["posts/29513.html","posts/35392.html","posts/55151.html","posts/52064.html","posts/35821.html","posts/33834.html","posts/30091.html","posts/23287.html","posts/35916.html","posts/11052.html","posts/35966.html"];function toRandomPost(){pjax.loadUrl("/"+posts[Math.floor(Math.random()*posts.length)])}
